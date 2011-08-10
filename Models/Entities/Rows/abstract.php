262517e1b86c72392e1766a3258ad7766099e06e
Israel Canasa <raeldc@gmail.com>
2011-08-10 11:15:26 +0800

    Improved row abstract

--- Models/Entities/Rows/abstract.php
+++ Models/Entities/Rows/abstract.php
@@ -2,24 +2,21 @@
 
 abstract class Models_Entities_Rows_Abstract {
     
-    protected $_modified = array();
+    protected $_columns = array();
     protected $_data = array();
     
     public function save()
     {
-        $this->_modified = array();
         return false;
     }
     
     public function __set($column, $value)
     {
-        if(!isset($this->_data[$column]) || ($this->_data[$column] != $value) || $this->isNew()) 
+        if(in_array($column, $this->_columns) && (!isset($this->_data[$column]) || ($this->_data[$column] != $value))) 
         {
             parent::__set($column, $value);
-
-            $this->_modified[$column] = true;
-            $this->_status            = null;
-        } 
+            $this->_data[$column] = $value;
+        }
     }
     
 }
