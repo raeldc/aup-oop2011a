74d3a37da42f7a5186c325dd6bb27c9f9da0818f
Israel Canasa <raeldc@gmail.com>
2011-08-10 11:15:48 +0800

    Create person row.

---
+++ Models/Entities/Rows/person.php
@@ -0,0 +1,14 @@
+<?php
+
+class Models_Entities_Rows_Person extends Models_Entities_Rows_Json 
+{
+    public function __construct()
+    {
+        $this->_columns = array(
+            'firstname',
+            'lastname',
+            'age',
+            'occupation'
+        );
+    }
+}
